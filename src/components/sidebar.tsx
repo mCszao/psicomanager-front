adf87015dc9f102fcdfede25c2380b0fa5683443
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-12 03:58:07 -0400

    chore: updating side bar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,12 +1,33 @@
+import { UsersRound, Home, BookOpenCheck, CalendarPlus, SmilePlus } from "lucide-react";
 import Link from "next/link";
 
 export default function SideLinks() {
     return (
-        <section className="bg-royalBlue p-5 flex flex-col fixed w-20">
-          <Link href={"/patients"}
-          className="group rounded-lg border border-transparent p-5 transition-colors bg-royalBlue hover:border-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        <section className="bg-royalBlue p-2 flex flex-col fixed m-r-5 w-20 h-full">
+          <Link href={"/"}
+          className="rounded-lg border border-transparent transition-colors p-5 hover:border-gray-300"
           >
-            Pacientes
+            <CalendarPlus color="white"/>
+          </Link>
+          <Link href={"/"}
+          className="rounded-lg border border-transparent transition-colors p-5 hover:border-gray-300"
+          >
+            <SmilePlus color="white"/>
+          </Link>
+          <Link href={"/"}
+          className="rounded-lg border border-transparent transition-colors p-5 hover:border-gray-300"
+          >
+            <Home color="white"/>
+          </Link>
+          <Link href={"/patient"}
+          className="rounded-lg border border-transparent transition-colors p-5 hover:border-gray-300"
+          >
+            <UsersRound color="white"/>
+          </Link>
+          <Link href={"/"}
+          className="rounded-lg border border-transparent transition-colors p-5 hover:border-gray-300"
+          >
+            <BookOpenCheck color="white"/>
           </Link>
       </section>
     )

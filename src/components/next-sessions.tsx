5bc20b7041cceb43cf0dfcaf527127563e08a0c3
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-12 03:59:32 -0400

    chore: using route fetch

--- src/components/next-sessions.tsx
+++ src/components/next-sessions.tsx
@@ -1,16 +1,15 @@
 import BaseResponse from "@/interface/IBaseResponse";
 import { Schedule } from "@/interface/ISchedule";
 import ItemScheduleSession from "./item-schedule-session";
+import {fetchSchedules} from "@/app/api/index";
+
 
 export default async function NextSessions(){
-    const request = await fetch('http://localhost:8080/schedules', {
-        headers: {Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwc2ljb21hbmFnZXItYXV0aC1sb2dpbiIsInN1YiI6ImtlbHppbmhhIiwiZXhwIjoxNzE4MTMyNTgxfQ.WI-hwsT50fZ8Gq6qI1VbyyXGe2nkpLb8QGa1i7a--Nk"}
-      })
-      let {object} =  await request.json() as BaseResponse<Schedule[]>;
+      const response = await fetchSchedules() as BaseResponse<Schedule[]>;      
     return (
         <div>
             <h2 className="text-3xl font-semibold text-royalBlue">Próximas Consultas</h2>
-            {object && object?.map(schedule => (
+            {response.object && response.object?.map(schedule => (
                  <ItemScheduleSession key={schedule.id}  data={schedule}/>
             ))}
         </div>

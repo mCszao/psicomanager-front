d5e8400afe81cd2b63936db9daf69465aff02b99
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-18 16:43:38 -0400

    feat: add components

---
+++ src/components/ui/dialog-header.tsx
@@ -0,0 +1,19 @@
+type Props = {
+    title: string;
+    textButton: React.ReactNode | string; 
+    functionButton: () => void;
+}
+
+export default function DialogHeader(props: Props) {
+    return (
+        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                {props.title}
+            </h3>
+            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" onClick={props.functionButton}>
+                {props.textButton}
+                <span className="sr-only">Close modal</span>
+            </button>
+        </div>
+    )
+}
---
+++ src/components/ui/dialog.tsx
@@ -0,0 +1,15 @@
+type Props = {
+    children: React.ReactNode;
+}
+
+export default function Dialog({ children }: Props)  {
+    return (
+        <div aria-hidden="true" className="ml-20 overflow-y-auto overflow-x-hidden fixed z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-opacity-10 bg-black/30">
+            <div className="relative p-4 w-full max-w-md max-h-full">
+                <div className="relative bg-white rounded-lg shadow ">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}

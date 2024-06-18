27e46214927943216e50185af8609e833283e798
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-18 16:44:08 -0400

    chore: change styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import NextSessions from "@/components/next-sessions";
-
+import metadataFactory from "@/util/metadataFactory";
+export const metadata = metadataFactory("Página Inicial");
 
 export default function Home() {
 
   return (
-    <main className="w-full">
-        <NextSessions/>
+    <main className="w-full pt-10 pl-24">
+      <h2 className="text-3xl font-semibold text-royalBlue">Próximas Consultas</h2>
+      <NextSessions/>
     </main>
   );
 }
--- src/components/next-sessions.tsx
+++ src/components/next-sessions.tsx
@@ -9,7 +9,7 @@ export default async function NextSessions(){
       let response = await fetchSchedules() as BaseResponse<Session[]>;      
     return (
         <div className="flex w-full min-h-screen flex-wrap p-24 pt-10 gap-5 text-[#fff]">
-            <section className="mb-32 mt-12  grid text-center gap-5 lg:mb-0 lg:w-full lg:max-w-7xl lg:grid-cols-4 lg:text-left">
+            <section className="mb-32 grid text-center gap-5 lg:mb-0 lg:w-full lg:max-w-7xl lg:grid-cols-4 lg:text-left">
                 {response.object && response.object?.map((schedule) => (
                     <ItemScheduleSession key={schedule.id}  data={schedule}/>
                 ))}

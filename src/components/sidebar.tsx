12a3233be5dca687554154314c24cb02f8681402
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-20 15:56:49 -0400

    chore: change style menu to be above all component

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,17 +1,22 @@
 'use client'
 import { UsersRound, Home, BookOpenCheck, CalendarPlus, SmilePlus } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
+import Dialog from "./ui/dialog";
+import DialogHeader from "./ui/dialog-header";
+import CreateSessionDialog from "./create-session-dialog";
 
 export default function SideLinks() {
+    const [isOpen, setOpen] = useState(true);
     function handleAddSession(){
-      alert("Clicou para adicionar uma nova sessão")
+      setOpen(!isOpen);
     }
 
     function handleAddPatient(){
       alert("Clicou para adicionar novo paciente")
     }
     return (
-        <section className="bg-royalBlue p-2 flex flex-col fixed m-r-5 w-20 h-full">
+        <section className="bg-royalBlue p-2 flex flex-col fixed m-r-5 w-20 h-full z-50">
           <button onClick={handleAddSession}
           className="rounded-lg border border-transparent transition-colors p-5 hover:border-gray-300"
           >
@@ -37,6 +42,9 @@ export default function SideLinks() {
           >
             <BookOpenCheck color="white"/>
           </Link>
+          {isOpen && (
+            <CreateSessionDialog/>
+          )}
       </section>
     )
 }

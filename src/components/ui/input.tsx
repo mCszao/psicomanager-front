45a6c122951a04a09f4388198ae90e22eb9a1ed9
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-21 01:59:16 -0400

    chore: adapting input to can be managed

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,11 +1,15 @@
-import { InputHTMLAttributes } from "react"
+import { useForm } from "react-hook-form";
+import { InputHTMLAttributes, forwardRef  } from "react"
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-    
 }
 
-export default function Input({ ...props }: Props) { 
-    return (
-        <input {...props} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
-    )
-}
+const Input = forwardRef<HTMLInputElement, Props>(
+    function CustomInput({...props}, ref) { 
+        return (
+            <input {...props} ref={ref} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
+        )
+    }
+)
+
+export default Input;

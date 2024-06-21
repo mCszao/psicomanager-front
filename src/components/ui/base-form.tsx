ecac932af0416fae757a059a6d3c735edacb22fb
Marcos Vinicius <marc.khk11@gmail.com>
2024-06-21 01:58:49 -0400

    chore: change styles and add general properties on base form

--- src/components/ui/base-form.tsx
+++ src/components/ui/base-form.tsx
@@ -1,10 +1,12 @@
-type Props = {
+import { FormHTMLAttributes } from "react";
+
+interface BaseFormProps extends FormHTMLAttributes<HTMLFormElement>{
     children: React.ReactNode;
 }
 
-export default function BaseForm({ children }: Props) {
+export default function BaseForm({ children, ...props }: BaseFormProps) {
     return (
-        <form className="p-4 md:p-5">
+        <form {...props} className="p-4 md:p-5">
             {children}
         </form>
     )
--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,12 +1,11 @@
 type Props = {
     children: React.ReactNode;
-    closeFunc: () => void;
 }
 
-export default function Dialog({ children, closeFunc }: Props)  {
+export default function Dialog({ children }: Props)  {
     return (
-        <div aria-hidden="true" className="ml-20 overflow-y-auto overflow-x-hidden fixed z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-opacity-10 bg-black/30" onClick={closeFunc}>
-            <div className="relative p-4 w-full max-w-md max-h-full">
+        <div aria-hidden="true" className="ml-20 overflow-y-auto overflow-x-hidden fixed z-40 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-opacity-10 bg-black/30">
+            <div className="relative p-4 w-full max-w-md max-h-full z-50">
                 <div className="relative bg-white rounded-lg shadow ">
                     {children}
                 </div>

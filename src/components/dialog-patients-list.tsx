de386763d2f8977ae180156840fa3c6fc044ffe6
mCszao <marc.khk11@gmail.com>
2024-07-03 00:42:32 -0400

    feat: new component to list patients on session dialog

---
+++ src/components/dialog-patients-list.tsx
@@ -0,0 +1,38 @@
+import { getPatients } from "@/services/api";
+import Dialog from "./ui/dialog";
+import DialogHeader from "./ui/dialog-header";
+import Input from "./ui/input";
+import BaseResponse from "@/interface/IBaseResponse";
+import { X } from "lucide-react";
+import { PatientResume } from "@/interface/IPatientResume";
+import { useEffect, useState } from "react";
+
+type Props = {
+    externalFunc: () => void;
+}
+
+export default function DialogPatientList({ externalFunc }: Props) {
+    const [patients, setPatients]= useState<PatientResume[] | null>(null);
+    useEffect(() => {
+        async function fetchData() {
+            // You can await here
+            const { success, object } = await getPatients() as BaseResponse<PatientResume[]>;
+            setPatients(object);
+        }
+        fetchData();
+      }, [])
+    return (
+        <Dialog>
+            <DialogHeader title="Pesquisar paciente" textButton={<X/>} functionButton={externalFunc}/>
+            <Input placeholder="Digite o nome do paciente"/>
+            <nav className="p-4">
+                {patients && patients.map((patient) : any => (
+                    <abbr key={patient.id} title={patient.email} className="p-5 block leading-tight transition-all outline-none  text-xl text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-900 focus:bg-blue-gray-500 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900 group bg-white hover:bg-royalBlue shadow-md  rounded-xl bg-clip-border cursor-pointer">
+                    {patient.name}
+                    </abbr>
+                ))}
+                
+            </nav>
+        </Dialog>
+    )
+}

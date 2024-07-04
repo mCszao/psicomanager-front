2b27fca2fcf747ef63f16301d8e0b2e719e7419a
mCszao <marc.khk11@gmail.com>
2024-07-04 01:29:51 -0400

    feat: parsing list client component to filter using onChange

--- src/app/patients/page.tsx
+++ src/app/patients/page.tsx
@@ -5,27 +5,16 @@ import { Cake } from "lucide-react";
 import Link from "next/link";
 import metadataFactory from "@/util/metadataFactory";
 import ContainerH6 from './../../components/ui/container-h6';
+import FilterPatientList from "@/components/filter-patient-list";
 
 export const metadata = metadataFactory("Lista de pacientes");
 
 export default async function Page(){
     let response = await getPatients() as BaseResponse<PatientResume[]>; 
     return (
-            <nav className="flex flex-col p-24 font-sans text-base font-normal gap-3 text-blue-gray-700 text-gray-700 w-full">
-                {response.object && response.object?.map((patient) : any => (
-                   <abbr key={patient.id} title={patient.email}>
-                      <Link href={"/patients/"+patient.id} key={patient.id} role="button"
-                      className="flex justify-between items-center p-5 leading-tight transition-all outline-none  text-4xl text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-900 focus:bg-blue-gray-500 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900 group bg-white hover:bg-royalBlue shadow-md  rounded-xl bg-clip-border ">
-                      <ContainerH6>
-                        {patient.name}
-                      </ContainerH6>
-                      <ContainerH6>
-                          <Cake/> 
-                          {patient.birthdayDate ?? 'Não informado'} 
-                      </ContainerH6>
-                    </Link>
-                   </abbr>
-                ))}     
-        </nav>
-    )
+              <>
+                <FilterPatientList data={response}/>
+              </>
+           
+            )
 }
---
+++ src/components/filter-patient-list.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import BaseResponse from "@/interface/IBaseResponse";
+import ContainerH6 from "./ui/container-h6";
+import { PatientResume } from "@/interface/IPatientResume";
+import Link from "next/link";
+import { Cake } from "lucide-react";
+import { useState } from "react";
+type Props = {
+    data : BaseResponse<PatientResume[]>
+}
+export default function FilterPatientList({ data }: Props) {
+    const [search, setSearch] = useState<string>('');
+    const filteredPatients =
+    (search.length != 0 && data != null)
+        ? data?.object.filter((patient) => {
+              const regex = new RegExp(search, 'i');
+              return patient?.['name'].match(regex);
+          })
+        : [];
+    
+    return (
+        <nav className="flex flex-col p-24 font-sans text-base font-normal gap-3 text-blue-gray-700 text-gray-700 w-full">
+            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" onChange={(e) => setSearch(e.target.value)} placeholder="Digite o nome do paciente"/> 
+                  {search.length > 0 ? filteredPatients?.map((patient) : any => (
+                    <abbr key={patient.id} title={patient.email}>
+                      <Link href={"/patients/"+patient.id} key={patient.id} role="button"
+                      className="flex justify-between items-center p-5 leading-tight transition-all outline-none  text-4xl text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-900 focus:bg-blue-gray-500 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900 group bg-white hover:bg-royalBlue shadow-md  rounded-xl bg-clip-border ">
+                        <ContainerH6>
+                          {patient.name}
+                        </ContainerH6>
+                        <ContainerH6>
+                          <Cake/> 
+                          {patient.birthdayDate ?? 'Não informado'} 
+                        </ContainerH6>
+                      </Link>
+                    </abbr>
+                  )) 
+                  :
+                    data.object?.map((patient) : any => (
+                    <abbr key={patient.id} title={patient.email}>
+                      <Link href={"/patients/"+patient.id} key={patient.id} role="button"
+                      className="flex justify-between items-center p-5 leading-tight transition-all outline-none  text-4xl text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-900 focus:bg-blue-gray-500 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900 group bg-white hover:bg-royalBlue shadow-md  rounded-xl bg-clip-border ">
+                        <ContainerH6>
+                          {patient.name}
+                        </ContainerH6>
+                        <ContainerH6>
+                          <Cake/> 
+                          {patient.birthdayDate ?? 'Não informado'} 
+                        </ContainerH6>
+                      </Link>
+                    </abbr>
+                  ))}     
+        </nav>
+    )
+}
